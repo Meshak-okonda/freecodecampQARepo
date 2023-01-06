949036190fb6ffb66ec9b8295c5ea90a851ccee4
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2023-01-06 12:44:46 +0100

    fix convert controllers

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -4,55 +4,82 @@ module.exports = function (req, res, next) {
 };
 
 function convert(data) {
-  const number = parseFloat(data) || 0;
-  if (data.indexOf("gal") > -1)
+  var matches = data.match(/[+-]?\d+(\.\d+)?/g);
+  let number = null;
+  if (matches) number = parseFloat(matches[0]);
+
+  const request = data.replace(matches[0], "").toLowerCase();
+
+  if (unitParse(request) == 0 && !number) return "invalid number and unit";
+
+  if (!number) return "invalid number";
+  if (unitParse(request) == 0) return "invalid unit";
+
+  if (unitParse(request) == 1)
     return {
       initNum: number,
       initUnit: "gal",
-      returnNum: number * 1.6,
+      returnNum: number * 3.78541,
       returnUnit: "L",
-      string: `${number} gal converts to ${(number * 3, 785)} L`,
+      string: `${number} gal to ${number * 3.78541} L`,
     };
 
-  if (data.indexOf("L") > -1)
+  if (unitParse(request) == 2)
     return {
       initNum: number,
       initUnit: "L",
-      returnNum: number * 1.6,
+      returnNum: number / 3.78541,
       returnUnit: "gal",
-      string: `${number} L converts to ${(number / 3, 785)} gal`,
+      string: `${number} L to ${number / 3.78541} gal`,
     };
-  if (data.indexOf("mi") > -1)
+  if (unitParse(request) == 3)
     return {
       initNum: number,
       initUnit: "mi",
       returnNum: number * 1.6,
       returnUnit: "km",
-      string: `${number} miles converts to ${number * 1.6} kilometers`,
+      string: `${number} mi to ${number * 1.6} km`,
     };
-  if (data.indexOf("km") > -1)
+  if (unitParse(request) == 4)
     return {
       initNum: number,
       initUnit: "km",
-      returnNum: number * 1.6,
+      returnNum: number / 1.6,
       returnUnit: "mi",
-      string: `${number} kilometers converts to ${number / 1.6} miles`,
+      string: `${number} km to ${number / 1.6} mi`,
     };
-  if (data.indexOf("lbs") > -1)
+  if (unitParse(request) == 5)
     return {
       initNum: number,
       initUnit: "lbs",
       returnNum: number / 2.205,
       returnUnit: "kg",
-      string: `${number} lbs converts to ${number / 2.205} kilogrammes`,
+      string: `${number} lbs to ${number / 2.205} kg`,
     };
 
-  if (data.indexOf("kg") > -1)
+  if (unitParse(request) == 6)
     return {
       initNum: number,
       initUnit: "kg",
-      returnNum: number / 2.205,
+      returnNum: number * 2.205,
       returnUnit: "lbs",
-      string: `${number} kilogrammes converts to ${number * 2.205} lbs`,
+      string: `${number} kg to ${number * 2.205} lbs`,
     };
 }
+
+const unitParse = (request) => {
+  console.log(request);
+  if (request == "kg") return 6;
+
+  if (request == "lbs") return 5;
+
+  if (request == "km") return 4;
+
+  if (request == "mi") return 3;
+
+  if (request == "l") return 2;
+
+  if (request == "gal") return 1;
+
+  return 0;
+};
